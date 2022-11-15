f3a87dcac1f1a7e24341f6fac0b08dfe87024ae7
Maceo <basse.maceo@gmail.com>
2022-11-15 14:57:23 +0100

    feat: add users services

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-// const usersRoutes = require("./routes/users.router");
+const usersRoutes = require("./routes/users.router");
 // const favorisRoutes = require("./routes/favoris.router");
 // const filmsRoutes = require("./routes/films.router");
 const recommandationsRoutes = require("./routes/recommandation.router");   
@@ -8,7 +8,7 @@ const recommandationsRoutes = require("./routes/recommandation.router");
 const app = express()
 app.use(bodyParser.json());
 
-// app.use("/users", usersRoutes);
+app.use("/users", usersRoutes);
 // app.use("/favoris", favorisRoutes);
 // app.use("/films", filmsRoutes);
 app.use("/recommandations", recommandationsRoutes);
