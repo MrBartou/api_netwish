54160b72bd82927fadcf288130805eeebdbb89af
DENIN <anthony.denin@cgi.com>
2022-11-25 16:30:34 +0100

    Add validator openapi

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const app = express();
 const swaggerUi = require('swagger-ui-express');
 YAML = require('yamljs')
 const swaggerFile = YAML.load('./openapi.yaml')
+const OpenApiValidator = require('express-openapi-validator');
+
 
 // const usersRoutes = require("./routes/users.router");
 // const favorisRoutes = require("./routes/favoris.router");
 // const filmsRoutes = require("./routes/films.router");
 // const recommandationsRoutes = require("./routes/recommandations.router");
 // const seriesRoutes = require("./routes/series.router");
-const app = express();
+
 app.use(bodyParser.json());
+
+app.use(
+    OpenApiValidator.middleware({
+      apiSpec: './openapi.yaml',
+      validateRequests: true,
+    }),
+);
 
 console.log("haaa");
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
@@ -19,5 +29,12 @@ app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 // app.use("/films", filmsRoutes);
 // app.use("/recommandations", recommandationsRoutes);
 // app.use("/series", seriesRoutes);
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+      message: err.message,
+      errors: err.errors,
+    });
+});
 
 module.exports = app;
