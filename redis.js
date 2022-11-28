b97361e07f9b111d2902036778bb03957af9ab93
Maceo <basse.maceo@gmail.com>
2022-11-28 15:05:15 +0100

    fix: fix redit host

--- redis.js
+++ redis.js
@@ -1,11 +1,9 @@
 const redis = require('redis');
 
 const client = redis.createClient({
-    url: process.env.REDIS_URL,
+    host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-
     legacyMode: true,
 });
-
 
 module.exports = client;
